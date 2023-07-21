ce6ad1b03697ffd8a8d1ce06daf33d06c0461ca4
Nagajyothi <n170264@rguktn.ac.in>
2023-07-22 02:28:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import React, { useState } from 'react';
 import './App.css';
 import Picture from './components/Picture';
 import ShoppingCart from './components/ShoppingCart';
+import flower1 from './Pictures/flower1.jpg';
+import flower2 from './Pictures/7.jpg';
+import flower3 from './Pictures/flower3.jpg';
+import flower4 from './Pictures/4.jpg';
+import flower5 from './Pictures/5.jpg';
+import flower6 from './Pictures/6.jpg';
+import flower7 from './Pictures/flower2.jpg';
+import flower8 from './Pictures/8.jpg';
+import flower9 from './Pictures/9.jpg';
+import flower10 from './Pictures/10.jpg';
+import flower11 from './Pictures/11.jpg';
 
 function App() {
   const initialPictures = [
-    { imageURL: '/Pictures/flower1.jpg', price: 10 },
-    { imageURL: '/Pictures/7.jpg', price: 15 },
-    { imageURL: '/Pictures/flower3.jpg', price: 5 },
-    { imageURL: '/Pictures/4.jpg', price: 10 },
-    { imageURL: '/Pictures/5.jpg', price: 15 },
-    { imageURL: '/Pictures/6.jpg', price: 5 },
-    { imageURL: '/Pictures/flower2.jpg', price: 10 },
-    { imageURL: '/Pictures/8.jpg', price: 15 },
-    { imageURL: '/Pictures/9.jpg', price: 5 },
-    { imageURL: '/Pictures/10.jpg', price: 10 },
-    { imageURL: '/Pictures/11.jpg', price: 15 },
+    { imageURL: flower1, price: 10 },
+    { imageURL: flower2, price: 15 },
+    { imageURL: flower3, price: 5 },
+    { imageURL: flower4, price: 10 },
+    { imageURL: flower5, price: 15 },
+    { imageURL: flower6, price: 5 },
+    { imageURL: flower7, price: 10 },
+    { imageURL: flower8, price: 15 },
+    { imageURL: flower9, price: 5 },
+    { imageURL: flower10, price: 10 },
+    { imageURL: flower11, price: 15 },
   ];
 
   const [selectedItems, setSelectedItems] = useState([]);
